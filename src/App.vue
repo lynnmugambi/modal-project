09580a192570b736bd7e0673604046f6ab4293db
Lynn Mugambi <mugambilynn@gmail.com>
2023-02-27 21:59:15 +0300

    custom events and modifiers

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1 class="example">{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :title=modalTitle :text=modalText theme="sale" @close="showModal = false" />
+  </div>
+  <button @click.alt="toggleModal"> Show Modal (alt)</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue3 App!',
+      modalTitle: 'This is a title',
+      modalText: 'This is a text',
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1.example {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1> {{ title }} </h1>
+      <p> {{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'text', 'theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+
+/* Components make code reusable, customizable. Props help to create a single source of truth. */
+
+</script>
+
+<style> /* You can use scoped css here to lock in the css variables, although consider this affects perfomance if you do it alot, it is minimal.
+The alternative is to use css specifiers that are unique to the component so even without the scoped keyword, it wont clash */
+ .modal {
+   width: 400px;
+   padding: 20px;
+   margin: 100px auto;
+   background: white;
+   border-radius: 10px;
+ }
+
+ .modal.sale {
+   background: red;
+   color: white;
+ }
+
+ .modal .h1 {
+   color: blue;
+ }
+
+ .backdrop {
+   top: 0;
+   position: fixed;
+   background: rgba(0, 0, 0, 0.5);
+   width: 100%;
+   height: 100%;
+ }
+</style>
